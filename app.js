d81c60e9ef411e54eb9264a000158dd9554a4d82
bun1t <b_un1t@hotmail.de>
2017-04-08 17:42:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 2743,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: '☆☆Un1t Dice☆☆',
+  app_name: 'Un1t Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph', // <----- EDIT ME!
